843ab175882b929fa9d00f97a7c6735ba530e910
kie2204 <51746856+kie2204@users.noreply.github.com>
2021-01-25 21:46:55 +0100

    Tilføj regler for flere elementer

--- grammatip-dark-mode.user.js
+++ grammatip-dark-mode.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Grammatip DarkMode
 // @namespace    https://kie2204.github.io/grammatip-dark-mode
-// @version      1.0
+// @version      1.1
 // @description  Undgå dårlige øjne med dette smarte UserScript til Grammatip!
 // @author       kie2204
 // @match        https://www.grammatip.com/*
@@ -37,10 +37,10 @@
     var sheet = window.document.styleSheets[0];
 
     // Smooth transition
-    sheet.insertRule('* { transition: background-color 0.25s; }', sheet.cssRules.length);
+    sheet.insertRule('* { transition: background-color 0.25s; color: white; }', sheet.cssRules.length);
 
     // Baggrund
-    sheet.insertRule('#main-content, .header, .nav-tabs > .active > a, .modal, .lp.background, .video-container .video-section, .user-settings-content { background-color: '+defaultPalette.background+' !important }', sheet.cssRules.length);
+    sheet.insertRule('#main-content, .header, .nav-tabs > .active > a, .modal, .lp.background, .video-container .video-section, .user-settings-content, select { background-color: '+defaultPalette.background+' !important }', sheet.cssRules.length);
 
     // Lyseblå
     sheet.insertRule('a { color: '+defaultPalette.lightBlue+' !important }', sheet.cssRules.length);
@@ -49,11 +49,11 @@
     sheet.insertRule('.header-logo a, .header-menu .current, a:hover { color: '+defaultPalette.darkBlue+' !important }', sheet.cssRules.length);
 
     // Tekst
-    sheet.insertRule('body, h1, h2, h3, h4, h5, h6, p, .nav-tabs > .active > a, li, input.dictationTextField { color: '+defaultPalette.text+' !important }', sheet.cssRules.length);
+    sheet.insertRule('body, h1, h2, h3, h4, h5, h6, p, .nav-tabs > .active > a, li, input.dictationTextField, label, form#exercise-form .punctuation-place, select { color: '+defaultPalette.text+' !important }', sheet.cssRules.length);
 
     // Kanter
-    sheet.insertRule('.nav > li > a:hover, .student-intro-exercise-box, .student-intro-feedback, input[type="text"] { background-color: '+defaultPalette.border+' !important }', sheet.cssRules.length);
-    sheet.insertRule('.nav-tabs, .student-stat .student-table td, .student-table, .nav-tabs > .active > a, .nav-tabs > li > a:hover, .student-intro-exercise-box, .student-intro-feedback, .modal-header, input.dictationTextField { border-color: '+defaultPalette.border+' !important }', sheet.cssRules.length);
+    sheet.insertRule('.nav > li > a:hover, .student-intro-exercise-box, .student-intro-feedback, input[type="text"], .header.frontpage-hd .gt-select, .header.frontpage-hd .gt-select .options, .gt-select .option { background-color: '+defaultPalette.border+' !important }', sheet.cssRules.length);
+    sheet.insertRule('.nav-tabs, .student-stat .student-table td, .student-table, .nav-tabs > .active > a, .nav-tabs > li > a:hover, .student-intro-exercise-box, .student-intro-feedback, .modal-header, input.dictationTextField, table.radio-table, table.checkbox-table, table.radio-table tr td, table.checkbox-table tr td, select, select * { border-color: '+defaultPalette.border+' !important }', sheet.cssRules.length);
     sheet.insertRule('.student-table, .nav-tabs > .active > a, .nav-tabs > li > a:hover { border-bottom-color: transparent !important }', sheet.cssRules.length);
 
     // Normal blå
@@ -62,5 +62,6 @@
     // Andre regler
     sheet.insertRule('.student-intro-img img { background-color: white !important; border-radius: 10px; padding: 10px 0 }', sheet.cssRules.length);
     sheet.insertRule('.student-intro-exercise-box, .student-intro-feedback { background: '+defaultPalette.border+' !important }', sheet.cssRules.length);
+    sheet.insertRule('span *, .notice *  { color: inherit; }', sheet.cssRules.length);
 
 })();
